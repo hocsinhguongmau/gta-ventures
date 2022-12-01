e566e11b9bff53cfc76feb33205d4c17b0f293c0
hocsinhguongmau <thangnguyen.webdev@gmail.com>
2022-12-01 14:54:38 +0200

    Add missing alt

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -17,12 +17,14 @@ export default function Layout({ children }) {
         height={363}
         width={363}
         className="absolute top-0 left-0 z-0 opacity-20"
+        alt=""
       />{' '}
       <Image
         src="/assets/images/bg-bot.png"
         height={363}
         width={363}
         className="absolute right-0 bottom-0 z-0 opacity-10"
+        alt=""
       />
       <div className="container relative z-10 mx-auto flex min-h-screen flex-col justify-between">
         <Header />
